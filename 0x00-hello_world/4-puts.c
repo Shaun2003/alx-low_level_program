e73922bb977c1959dec727cd528cd1497b7fa725
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-02-16 11:20:12 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A Program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
