611170e339c4986416b17e438840874c801b21af
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-03-02 14:16:56 -0800

    101 print: '  ' : ' ; /
    ;
    [

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,7 +14,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		nl = -n;
 	}
 
@@ -22,5 +22,5 @@ void print_number(int n)
 	{
 		print_number(nl / 10);
 	}
-	_putchar((nl % 10) + '0');
+	putchar((nl % 10) + '0');
 }
