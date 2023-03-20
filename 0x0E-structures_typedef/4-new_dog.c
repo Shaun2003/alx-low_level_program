dfff6109a33dbad1913bed489dd0cdfebc9f7e9d
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-03-20 14:17:46 -0700

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,24 +10,31 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	return (i);
 }
+
 /**
  * _strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ *
+ * Return: 0
  */
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
+
 	while (src[len] != '\0')
 	{
 		len++;
@@ -39,6 +46,7 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -51,6 +59,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
